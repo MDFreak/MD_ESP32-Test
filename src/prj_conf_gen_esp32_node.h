7538c1f532940f2a41edb076c21b9ec1219d29c8
MDFreak_ESP32 <post@martin-dorfner.at>
2022-12-12 22:01:15 +0100

    --- Auto Git Commit ---

--- src/prj_conf_gen_esp32_node.h
+++ src/prj_conf_gen_esp32_node.h
@@ -704,12 +704,12 @@
         #endif
 
       #if (USE_PHOTO_SENS > OFF)
-        #define PHOTO_FILT        7
-        #define PHOTO_DROP        0
-        #define ATT_PHOTO_SENS    3 // 2 = ADC_ATTEN_DB_6; 3 = ADC_ATTEN_DB_11
-        #ifndef USE_MEASURE_CYCLE
-            #define USE_MEASURE_CYCLE
-          #endif
+          #define PHOTO_FILT        7
+          #define PHOTO_DROP        0
+          #define ATT_PHOTO_SENS    3 // 2 = ADC_ATTEN_DB_6; 3 = ADC_ATTEN_DB_11
+          #ifndef USE_MEASURE_CYCLE
+              #define USE_MEASURE_CYCLE
+            #endif
         #endif
 
       #define ANZ_ANASENS  USE_DS18B20_1W_IO + USE_BME280_I2C * 3 + USE_MQ135_GAS_ADC + USE_TYPE_K_SPI
