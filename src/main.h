e4e48fdc6642f755125f3eb1c671d603d0526bd1
MDFreak_ESP32 <post@martin-dorfner.at>
2023-01-28 20:55:39 +0100

    --- Auto Git Commit ---

--- src/main.h
+++ src/main.h
@@ -185,7 +185,7 @@
         #include <md_webserver.h>
         #if (USE_MQTT > OFF)
             //#include <espMqttClient.h>
-            #include "./../md_lib/md_eMQTT5lib/include/Network/Clients/MQTT.hpp"
+            #include <MQTT.hpp>
           #endif
       #endif
   // --- sensors
