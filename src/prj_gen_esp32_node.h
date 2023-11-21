85ec4ad9023269507c2948556e25b8aaf34ab05d
MDFreak <inbox@martin-dorfner.at>
2023-11-21 19:41:19 +0100

    --- Auto Git Commit ---

--- src/prj_gen_esp32_node.h
+++ src/prj_gen_esp32_node.h
@@ -7,7 +7,7 @@
 
   // ******************************************
     // --- debugging
-      #define DEBUG_MODE      CFG_DEBUG_STARTUP
+        //#define DEBUG_MODE      CFG_DEBUG_STARTUP
         //#define DEBUG_MODE      CFG_DEBUG_NONE
         //#define DEBUG_MODE      CFG_DEBUG_ACTIONS
         //#define DEBUG_MODE      CFG_DEBUG_DETAILS
@@ -23,55 +23,55 @@
       //#define DEV_PWM_OUTPUT        ON
       //#define DEV_ADC_INT           1
     // --- user output components
-      #define USE_WS2812_PWR_IN_SW  OFF                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         // some time matrix and line not allowed
-      // --- displays
-        #define USE_DISP            1
-          // OLEDs
-            #define USE_OLED_I2C    1
-          // TFTs
-            #define USE_DISP_TFT    OFF
-              // MC_UO_TFT1602_GPIO_RO, MC_UO_TOUCHXPT2046_AZ_UNO, MC_UO_TXPT2046_AZ_SPI
-              // MC_UO_TFT1602_I2C_XA,  MC_UO_Keypad_ANA0_RO
-      // --- acustic output
-        #define USE_AOUT              OFF
-          // buzzer
-            #define BUZZER1           OFF
-            // AOUT_PAS_BUZZ_3V5V
-      // --- PWM output
-        #define USE_FAN_PWM           1   // 2
-        #define USE_OUT_FREQ_PWM      OFF // 1
-      // --- LED output
-        #define USE_TRAFFIC_LED_OUT   OFF
-        #define USE_RGBLED_PWM        1
-        #define USE_WS2812_MATRIX_OUT OFF // [0, 1..4]
-        #define USE_WS2812_LINE_OUT   OFF // [0, 1..4]
-      // --- digital output
-        #define USE_GEN_DIG_OUT       1
+      //#define USE_WS2812_PWR_IN_SW  OFF                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         // some time matrix and line not allowed
+      //// --- displays
+      //  #define USE_DISP            1
+      //    // OLEDs
+      //      #define USE_OLED_I2C    1
+      //    // TFTs
+      //      #define USE_DISP_TFT    OFF
+      //        // MC_UO_TFT1602_GPIO_RO, MC_UO_TOUCHXPT2046_AZ_UNO, MC_UO_TXPT2046_AZ_SPI
+      //        // MC_UO_TFT1602_I2C_XA,  MC_UO_Keypad_ANA0_RO
+      //// --- acustic output
+      //  #define USE_AOUT              OFF
+      //    // buzzer
+      //      #define BUZZER1           OFF
+      //      // AOUT_PAS_BUZZ_3V5V
+      //// --- PWM output
+      //  #define USE_FAN_PWM           1   // 2
+      //  #define USE_OUT_FREQ_PWM      OFF // 1
+      //// --- LED output
+      //  #define USE_TRAFFIC_LED_OUT   OFF
+      //  #define USE_RGBLED_PWM        1
+      //  #define USE_WS2812_MATRIX_OUT OFF // [0, 1..4]
+      //  #define USE_WS2812_LINE_OUT   OFF // [0, 1..4]
+      //// --- digital output
+      //  #define USE_GEN_DIG_OUT       1
     // --- user input components
       // --- touchscreen ---
-        #define USE_TOUCHSCREEN     OFF
-        // MC_UI_TXPT2046_AZ_SPI, MC_UI_TXPT2046_AZ_UNO
+        //#define USE_TOUCHSCREEN     OFF
+        //// MC_UI_TXPT2046_AZ_SPI, MC_UI_TXPT2046_AZ_UNO
       // --- keypads ---
-        #define USE_KEYPADSHIELD    OFF
-        // MC_UI_Keypad_ANA0_RO
-      #define USE_GEN_SW_INP        OFF   // 1
-      #define USE_GEN_CNT_INP       OFF // 1
-      #define USE_GEN_PWM_INP       OFF // 2
+        //#define USE_KEYPADSHIELD    OFF
+        //// MC_UI_Keypad_ANA0_RO
+      //#define USE_GEN_SW_INP        OFF   // 1
+      //#define USE_GEN_CNT_INP       OFF // 1
+      //#define USE_GEN_PWM_INP       OFF // 2
     // --- sensors
-      #define USE_ESPHALL           OFF
-      #define USE_DS18B20_1W_IO     2   // [0, 1, ....] limited by 1W connections
-      #define USE_CCS811_I2C        0   // [0, 1, ....] limited by I2C channels/addr
-      #define USE_BME280_I2C        1   // [0, 1, ....] limited by I2C channels/addr
-      #define USE_TYPE_K_SPI        OFF // [0, 1, ....] limited by Pins
-      #define USE_MQ135_GAS_ANA     OFF // [0, 1, ....] limited by analog inputs
-      #define USE_MQ3_ALK_ANA       OFF // ADC or ADS1115
-      #define USE_PHOTO_SENS_ANA    1   // ADC or ADS1115
-      #define USE_INA3221_I2C       1
-      #define USE_ACS712_ANA        1   // only ADS1115
-      #define USE_POTI_ANA          1   // ADC or ADS1115
-      #define USE_VCC50_ANA         1   // ADC or ADS1115
-      #define USE_VCC33_ANA         1   // ADC or ADS1115
-      #define USE_ADC1115_I2C       1   // ADC 4 channels 16Bit
+      //#define USE_ESPHALL           OFF
+      //#define USE_DS18B20_1W_IO     2   // [0, 1, ....] limited by 1W connections
+      //#define USE_CCS811_I2C        0   // [0, 1, ....] limited by I2C channels/addr
+      //#define USE_BME280_I2C        1   // [0, 1, ....] limited by I2C channels/addr
+      //#define USE_TYPE_K_SPI        OFF // [0, 1, ....] limited by Pins
+      //#define USE_MQ135_GAS_ANA     OFF // [0, 1, ....] limited by analog inputs
+      //#define USE_MQ3_ALK_ANA       OFF // ADC or ADS1115
+      //#define USE_PHOTO_SENS_ANA    1   // ADC or ADS1115
+      //#define USE_INA3221_I2C       1
+      //#define USE_ACS712_ANA        1   // only ADS1115
+      //#define USE_POTI_ANA          1   // ADC or ADS1115
+      //#define USE_VCC50_ANA         1   // ADC or ADS1115
+      //#define USE_VCC33_ANA         1   // ADC or ADS1115
+      //#define USE_ADC1115_I2C       1   // ADC 4 channels 16Bit
     // --- network  components
       #define USE_WIFI              ON  // ON
       #define USE_NTP_SERVER        ON  // ON
